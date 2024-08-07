2a0a94e2ca58a4f27d7a7387b8a0be934d668b02
jdquinterocg <jdquinterocg@gmail.com>
2024-08-06 20:53:04 -0500

    REMOVE unnecessary logs

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,8 +2,6 @@ export const fetchArticles = async () => {
     const url = import.meta.env.VITE_API_URL;
     const token = import.meta.env.VITE_API_TOKEN;
 
-    console.log({url, token})
-
     const response = await fetch(url, {
       headers: {
         authentication: token
